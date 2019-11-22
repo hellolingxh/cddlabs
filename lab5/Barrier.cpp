637bce6e361ab52be0d8d164d6814c842530e43c
Xiaohui <hellolingxh@gmail.com>
2019-11-22 23:46:06 +0000

    add the document generated by doxygen

--- lab5/Barrier.cpp
+++ lab5/Barrier.cpp
@@ -1,47 +1,34 @@
 // Barrier.cpp --- 
 // 
 // Filename: Barrier.cpp
-// Description: 
-// Author: Joseph
-// Maintainer: 
-// Created: Tue Jan  8 12:14:02 2019 (+0000)
-// Version: 
-// Package-Requires: ()
-// Last-Updated: Tue Jan  8 12:15:21 2019 (+0000)
-//           By: Joseph
-//     Update #: 2
-// URL: 
-// Doc URL: 
-// Keywords: 
-// Compatibility: 
+// Description: As one of the synchronization methods, a barrier tells a group of threads or processes must stop at the barrier and cannot proceed until all other threads/processes reach this barrier.
+// Author: Xiaohui Ling 
+// Created:22/11/2019 
 // 
-// 
+//The MIT License
 
-// Commentary: 
-// 
-// 
-// 
-// 
-
-// Change Log:
-// 
-// 
-// 
-// 
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or (at
-// your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
-// 
-// 
+/* * Copyright (c) 2019 Xiaohui Ling
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */ 
 
 // Code:
 #include "Semaphore.h"
---
+++ lab5/docs/html/class_barrier.js
@@ -0,0 +1,8 @@
+var class_barrier =
+[
+    [ "Barrier", "class_barrier.html#a7290fb8952d0f7779b8d6a7a34bbd407", null ],
+    [ "~Barrier", "class_barrier.html#a401f40e73302009b305904ffc7825304", null ],
+    [ "phase1", "class_barrier.html#aef1a70f9dfdaa5f0a10274a46a040f8d", null ],
+    [ "phase2", "class_barrier.html#ac91842c7f750131e80a037a02feb5486", null ],
+    [ "wait", "class_barrier.html#a83a9d2e85e98b3d2081538bf0da29b60", null ]
+];
